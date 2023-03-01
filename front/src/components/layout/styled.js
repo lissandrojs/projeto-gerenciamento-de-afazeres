8e7c3a0875e73b5720abdddabcbd079c49d7216f
Joao Morais <joaomoraisxd@gmail.com>
2023-03-01 18:54:06 -0300

    teste

--- front/src/components/layout/styled.js
+++ front/src/components/layout/styled.js
@@ -2,9 +2,10 @@ import styled from 'styled-components'
 
 export const Header = styled.header`
     width: 100%;
-    height: 40px;
+    height: 30px;
     background-color: #000;
 `
 
 export const Footer = styled.footer`
+
 `
